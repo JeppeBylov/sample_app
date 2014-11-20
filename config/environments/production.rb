1fc7630a7475c4c12cb623b80b29e2d27772df65
Jeppe Nielsen <code@bylov.net>
2014-11-20 19:51:15 +0100

    Edit production environment config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     :authentication   => :plain,
     :user_name        => ENV['SENDGRID_USERNAME'],
     :password         => ENV['SENDGRID_PASSWORD']
-    :domain           => 'bylov.net',
+    :domain           => 'heroku.com',
     :enable_starttls_auto => true
   }
 
